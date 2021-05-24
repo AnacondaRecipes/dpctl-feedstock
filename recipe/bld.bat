1843108f6ff3b0b4c6984cf3a845fd8237b68f39
Anthony DiPietro <adipietro@anaconda.com>
2021-05-24 19:08:32 +0000

    Initial commit of dpctl recipe.

---
+++ recipe/bld.bat
@@ -0,0 +1,17 @@
+call "%ONEAPI_ROOT%\compiler\latest\env\vars.bat"
+if errorlevel 1 (
+    echo "oneAPI compiler activation failed"
+    exit /b 1
+)
+
+"%PYTHON%" setup.py clean --all
+"%PYTHON%" setup.py install
+if errorlevel 1 exit 1
+
+rem Build wheel package
+if NOT "%WHEELS_OUTPUT_FOLDER%"=="" (
+    %PYTHON% setup.py bdist_wheel
+    if errorlevel 1 exit 1
+    copy dist\dpctl*.whl %WHEELS_OUTPUT_FOLDER%
+    if errorlevel 1 exit 1
+)
---
+++ recipe/run_test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e
+
+# Suppress error b/c it could fail on Ubuntu 18.04
+source ${ONEAPI_ROOT}/compiler/latest/env/vars.sh || true
+
+${PYTHON} -c "import dpctl"
+pytest -q -ra --disable-warnings --cov dpctl --cov-report term-missing --pyargs dpctl -vv
