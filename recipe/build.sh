0808776a0abaae7cdbe5967c6c6f0a2572fbaff4
Charles Bousseau <cbousseau@anaconda.com>
2022-10-27 14:04:26 -0400

    correct path

--- recipe/build.sh
+++ recipe/build.sh
@@ -16,5 +16,5 @@ ${PYTHON} setup.py install --single-version-externally-managed --record=record.t
 for CHANGE in "activate" "deactivate"
 do
     mkdir -p "${PREFIX}/etc/conda/${CHANGE}.d"
-    cp "${RECIPE_DIR}/${CHANGE}.sh" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.sh"
+    cp "${RECIPE_DIR}/scripts/${CHANGE}.sh" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.sh"
 done
