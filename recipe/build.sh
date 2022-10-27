e420f06c057dd87a9cc83078a2897e26e4f0c08a
Charles Bousseau <cbousseau@anaconda.com>
2022-10-27 13:55:49 -0400

    copy activate script to package

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,3 +10,11 @@ SKBUILD_ARGS="-- -DCMAKE_C_COMPILER:PATH=icx -DCMAKE_CXX_COMPILER:PATH=icpx"
 
 # Perform regular install
 ${PYTHON} setup.py install --single-version-externally-managed --record=record.txt ${SKBUILD_ARGS}
+
+# Copy the [de]activate scripts to $PREFIX/etc/conda/[de]activate.d.
+# This will allow them to be run on environment activation.
+for CHANGE in "activate" "deactivate"
+do
+    mkdir -p "${PREFIX}/etc/conda/${CHANGE}.d"
+    cp "${RECIPE_DIR}/${CHANGE}.sh" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.sh"
+done
