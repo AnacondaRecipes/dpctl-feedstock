df9e8c82e45ba48b56f0fd313ac58d9392cd9c0c
Charles Bousseau <cbousseau@anaconda.com>
2022-11-02 14:38:38 -0400

    update build

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-set -e
+set -ex
 
+export OCL_ICD_FILENAMES=libintelocl.so
+export SYCL_ENABLE_HOST_DEVICE=1
+export SYCL_PI_TRACE=1
 ${PYTHON} -c "import dpctl; print(dpctl.__version__)"
 ${PYTHON} -c "import dpctl; dpctl.lsplatform()"
 ${PYTHON} -m pytest -q -ra --disable-warnings -p no:faulthandler --cov dpctl --cov-report term-missing -k "not test_get_dpcppversion" --pyargs dpctl -vv
